d40f64f5a1e456f0080e669d7310d2da665ff539
Nick Cao <nickcao@nichi.co>
2025-03-27 09:59:55 -0400

    Update

--- install.sh
+++ install.sh
@@ -1,41 +1,19 @@
 #!/usr/bin/env bash
 set -euxo pipefail
 # create kind cluster
-# kind create cluster --config kind.yaml
-
-# install autocert
-helm upgrade --install autocert autocert \
-  --namespace autocert --create-namespace \
-  --repo https://smallstep.github.io/helm-charts \
-  --wait --wait-for-jobs
+kind create cluster --config kind.yaml
 
 # allow unauthenticated access to oidc endpoint
 kubectl create clusterrolebinding oidc-reviewer  \
   --clusterrole=system:service-account-issuer-discovery \
-  --group=system:unauthenticated
-
-# install dex
-kubectl create namespace dex
-kubectl label  namespace dex autocert.step.sm=enabled
-helm upgrade --install dex dex \
-  --repo https://charts.dexidp.io \
-  --namespace dex --create-namespace \
-  -f dex.values.yaml \
-  --wait --wait-for-jobs
+  --group=system:unauthenticated || true
 
 # install jumpstarter
-kubectl create namespace jumpstarter-lab
-kubectl label  namespace jumpstarter-lab autocert.step.sm=enabled
 helm upgrade --install jumpstarter oci://quay.io/jumpstarter-dev/helm/jumpstarter \
-  --version=0.5.0-114-g530557a \
+  --version=0.5.0-117-g1b98390 \
   --namespace jumpstarter-lab --create-namespace \
-  -f jumpstarter.values.yaml \
+  -f values.yaml \
   --wait --wait-for-jobs
 
-# install nginx ingress controller
-# helm upgrade --install ingress-nginx ingress-nginx \
-#   --repo https://kubernetes.github.io/ingress-nginx \
-#   --namespace ingress-nginx --create-namespace \
-#   --set controller.service.type=NodePort \
-#   --set controller.config.worker-processes=2 \
-#   --wait --wait-for-jobs
+# deploy qemu exporters
+kubectl apply -n default -f qemu-exporter-statefulset.yaml
